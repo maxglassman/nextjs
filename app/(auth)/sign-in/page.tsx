1b5cefe70b5ce6e96295ada7a864c3d0966a4841
Max Glassman <maxglassman@Maxs-MacBook-Pro.local>
2023-12-10 14:31:58 -0500

    added auth

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import page from "@/app/page";
-import { sign } from "crypto";
-import React from "react";
-
-const SignIn = () => {
-  return <div>sign-in</div>;
-};
-
-export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import page from "@/app/page";
-import { sign } from "crypto";
-import React from "react";
-
-const SignUp = () => {
-  return <div>sign-up</div>;
-};
-
-export default SignUp;
