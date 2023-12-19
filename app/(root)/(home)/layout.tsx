871401283a476586b2132908086bfeac41572b9b
Max Glassman <maxglassman@Maxs-MacBook-Pro.local>
2023-12-19 15:20:05 -0600

    completed first iteration of nav bar.

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "@/components/shared/navbar/NavBar";
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      <NavBar />
+      <div className="flex">
+        LeftSidebar
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5x1">{children}</div>
+        </section>
+        RightSidebar
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
