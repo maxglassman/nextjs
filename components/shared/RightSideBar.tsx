fe984252671d54a549bfa6d63abc47b12381f1d7
Max Glassman <maxglassman@Maxs-MacBook-Pro.local>
2023-12-26 13:46:45 -0600

    Created a right side bar.

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -2,36 +2,52 @@ import Link from "next/link";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import React from "react";
+import RenderTag from "./RenderTag";
 
-//TODO:
-// Create div for right side bar
-// Create 2 sections for right side bar, Top Questions and Popular Tags
-// Top Questions section has to map through the top questions array and display the top questions, Link, Text and arrow image
-// Popular Tags section has to map through the popular tags array and display the popular tags, Link, Badge, text
+const dummyTags = [
+  {
+    _id: 1,
+    name: "Javascript",
+    totalQuestions: 1234,
+    showTotalQuestions: true,
+  },
+  { _id: 2, name: "Python", totalQuestions: 1234, showTotalQuestions: true },
+  { _id: 3, name: "C++", totalQuestions: 1234, showTotalQuestions: true },
+  { _id: 4, name: "React.JS", totalQuestions: 1234, showTotalQuestions: true },
+];
 
 const TopQuestions = () => {
   const dummyQuestions = [
     {
       qid: 1,
-      question: "Dummy Question 1",
-      link: "/dummy",
+      question:
+        "Would it be appropriate to point out an error in another paper during a referee report?",
     },
-    { qid: 2, question: "Dummy Question 2", link: "/dummy" },
-    { qid: 3, question: "Dummy Question 3", link: "/dummy" },
-    { qid: 4, question: "Dummy Question 4", link: "/dummy" },
-    { qid: 5, question: "Dummy Question 5", link: "/dummy" },
+    {
+      qid: 2,
+      question: "How can an airconditioning machine exist?",
+    },
+    {
+      qid: 3,
+      question: "Interrogated every time crossing UK Border as citizen",
+    },
+    { qid: 4, question: "Low digit addition generator", link: "/dummy" },
+    {
+      qid: 5,
+      question: "What is an example of 3 numbers that do not make up a vector?",
+    },
   ];
 
   return (
-    <section className="flex flex-col gap-[30px]">
+    <div className="mt-7 w-full flex flex-col gap-[30px]">
       {dummyQuestions.map((question) => {
         return (
           <Link
             key={question.qid}
-            href={question.link}
-            className="flex flex-row gap-5"
+            href={`/questions/${question.qid}`}
+            className="flex cursor-pointer items-center justify-between flex-row gap-7"
           >
-            <p className="font-inter max-w-[248px] text-dark500_light700">
+            <p className="body-medium text-dark500_light700">
               {question.question}
             </p>
             <Image
@@ -44,42 +60,28 @@ const TopQuestions = () => {
           </Link>
         );
       })}
-    </section>
+    </div>
   );
 };
 
-const PopularTags = () => {
-  const dummyTags = [
-    { tag: "Javascript", count: 1234, url: "/dummy" },
-    { tag: "Python", count: 1234, url: "/dummy" },
-    { tag: "C++", count: 1234, url: "/dummy" },
-    { tag: "React.JS", count: 1234, url: "/dummy" },
-  ];
-  return (
-    <section className="flex flex-col gap-[30px]">
-      {dummyTags.map((tag) => {
-        return (
-          <Link key={tag.tag} href={tag.url} className="flex flex-row gap-5">
-            <Badge>{tag.tag}</Badge>
-            <p className="font-inter max-w-[248px] text-dark500_light700">
-              {tag.count}
-            </p>
-          </Link>
-        );
-      })}
-    </section>
-  );
-};
 const RightSideBar = () => {
   return (
-    <div className=" custom-scrollbar light-border border-left overflow-y-auto background-light900_dark200 flex flex-col justify-between h-screen lg:w-fit max-lg:hidden pt-[164px] pb-[50px] px-4">
-      <h3 className="text-dark200_light900 h3-bold font-inter">
-        Top Questions
-      </h3>
-      <TopQuestions />
-      <h3 className="text-dark200_light900 h3-bold font-inter">Popular Tags</h3>
-      <PopularTags />
-    </div>
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="text-dark200_light900 h3-bold">Top Questions</h3>
+        <TopQuestions />
+      </div>
+      <div className="mt-16">
+        <h3 className="text-dark200_light900 h3-bold font-inter">
+          Popular Tags
+        </h3>
+        <section className="mt-7 w-full flex flex-col gap-[30px]">
+          {dummyTags.map((tag) => {
+            return <RenderTag key={tag._id} {...tag} />;
+          })}
+        </section>
+      </div>
+    </section>
   );
 };
 
