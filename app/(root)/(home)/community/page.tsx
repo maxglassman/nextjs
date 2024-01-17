fb846a74c837b5ef69a63ff9a5b02cc1f5abb3e4
Max Glassman <maxglassman@Maxs-MacBook-Pro.local>
2024-01-17 16:23:17 -0500

    completed first iteration of community page.

--- app/(root)/(home)/community/page.tsx
+++ app/(root)/(home)/community/page.tsx
@@ -1,7 +1,46 @@
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import Filters from "@/components/shared/filters/Filters";
+import { UserFilters } from "@/constants/filters";
 import React from "react";
+import { getAllUsers } from "@/lib/actions/user.action";
+import UserCard from "@/components/shared/cards/UserCard";
+import { useSearchParams } from "next/navigation";
+import fetch from "node-fetch";
 
-const page = () => {
-  return <div>Community</div>;
+const page = async () => {
+  //get all users from mongodb
+
+  const users = await getAllUsers({});
+
+  const userLength = 50;
+  while (users.length < userLength) {
+    users.push(users[0]);
+  }
+
+  return (
+    <div>
+      <h1 className="h1-bold">All Users</h1>
+      <div className="mt-[30px] gap-3 sm:flex">
+        <LocalSearch
+          route="community"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search by Username"
+          otherClasses="flex-1 min-h-[56px]"
+        />
+        <Filters
+          filters={UserFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px] max-sm:mt-2"
+          containerClasses=""
+        />
+      </div>
+      <section className="w-full flex flex-row flex-wrap justify-left gap-2 mt-7">
+        {users.map((user) => {
+          return <UserCard key={user._id} user={user} />;
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default page;
---
+++ components/shared/cards/UserCard.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import RenderTag from "../RenderTag"; // Import the RenderTag component
+import Link from "next/link";
+import Image from "next/image";
+import { getTopInteractedTagsParams } from "@/lib/actions/tag.action";
+import { Badge } from "@/components/ui/badge";
+
+// Define the types for the props
+interface UserCardProps {
+  user: {
+    _id: string;
+    clerkId: string;
+    picture: string;
+    name: string;
+    username: string;
+  };
+}
+
+const UserCard = async ({ user }: UserCardProps) => {
+  const interactedTags = await getTopInteractedTagsParams({ userId: user._id });
+
+  return (
+    <Link
+      href={`/profile/${user.clerkId}`}
+      className="background-light900_dark200 border light-border rounded-lg shadow-md p-6 text-center w-[266px]"
+    >
+      <article>
+        <Image
+          src={user.picture}
+          alt={`Avatar of ${user.name}`}
+          className="rounded-full mx-auto"
+          height={100}
+          width={100}
+        />
+        <div className="mt-4">
+          <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+            {user.name}
+          </h3>
+          <p className="body-regular text-dark500_light500 mt-2 ">
+            @{user.username}
+          </p>
+        </div>
+        <div className=" mt-4">
+          {interactedTags !== undefined ? (
+            <div className="flex flex-wrap justify-center gap-2">
+              {interactedTags.map((tag) => {
+                return (
+                  <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+                );
+              })}
+            </div>
+          ) : (
+            <Badge>No Tags</Badge>
+          )}
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+export default UserCard;
